2530011ddf72981662f296a690c79e21939d16a8
danielfelipetorreslopez <danielfelipetorreslopez@gmail.com>
2024-05-25 17:52:56 -0500

    implementando proyecyo en clientes

--- clientes/manage.py
+++ clientes/manage.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import os
 import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "monitoring.settings")
