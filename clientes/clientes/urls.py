2f24b782521db074deaeb60fc61ae61d73b5fd32
danielfelipetorreslopez <danielfelipetorreslopez@gmail.com>
2024-05-25 17:04:53 -0500

    aa

--- clientes/clientes/urls.py
+++ clientes/clientes/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path, include
-from django.conf.urls import url, include
+#from django.conf.urls import url, include
 from django.views.decorators.csrf import csrf_exempt
 
 from . import views
 
 urlpatterns = [
     path('clientes/', views.cliente_list, name='clienteList'),
-    url(r'^clientes/', views.ClienteList),
-    url(r'^clientecreate/$', csrf_exempt(views.ClienteCreate), name='clienteCreate'),
-    url(r'^createclientes/$', csrf_exempt(views.ClientesCreate), name='createClientes'),
+    #url(r'^clientes/', views.ClienteList),
+    #url(r'^clientecreate/$', csrf_exempt(views.ClienteCreate), name='clienteCreate'),
+    #url(r'^createclientes/$', csrf_exempt(views.ClientesCreate), name='createClientes'),
 ]
