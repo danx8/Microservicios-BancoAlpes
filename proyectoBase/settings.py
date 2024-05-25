4c0ce567a836c5044f0357b0649bbabcd91212c8
danielfelipetorreslopez <danielfelipetorreslopez@gmail.com>
2024-05-25 16:28:35 -0500

    a

--- proyectoBase/settings.py
+++ proyectoBase/settings.py
@@ -38,8 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'clientes',
-    'propuestas', 
+    'clientes', 
     'social_django',
     
 ]
