a62c8ceb4af9aeccd14494e78054b479cecaaaa4
danielfelipetorreslopez <danielfelipetorreslopez@gmail.com>
2024-05-26 09:22:07 -0500

    a

--- clientes/clientes/urls.py
+++ clientes/clientes/urls.py
@@ -6,15 +6,15 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
+
+
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('clientes/', views.cliente_list, name='clienteList'),
     path(r'', include('django.contrib.auth.urls')),
-    
-    path(r'', include('.urls')),
-    
+        
     #url(r'^clientes/', views.ClienteList),
     #url(r'^clientecreate/$', csrf_exempt(views.ClienteCreate), name='clienteCreate'),
     #url(r'^createclientes/$', csrf_exempt(views.ClientesCreate), name='createClientes'),
