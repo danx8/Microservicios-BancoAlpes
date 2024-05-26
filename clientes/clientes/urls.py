1d1a4b8a52e61db303de253b667bc17b33f22361
danielfelipetorreslopez <danielfelipetorreslopez@gmail.com>
2024-05-26 09:33:09 -0500

    a

--- clientes/clientes/urls.py
+++ clientes/clientes/urls.py
@@ -2,7 +2,6 @@ from django.urls import path, include
 #from django.conf.urls import url, include
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib import admin
-from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
