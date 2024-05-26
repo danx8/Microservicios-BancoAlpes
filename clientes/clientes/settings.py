96e1c6219eab3e2aac1cf2aabb923ba48987c455
danielfelipetorreslopez <danielfelipetorreslopez@gmail.com>
2024-05-26 10:07:11 -0500

    a

--- clientes/clientes/settings.py
+++ clientes/clientes/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import jwt
+
+JWT_ALGORITHM = 'RS256'
+JWT_LEEWAY = 60  # Leeway in seconds to account for clock skew
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
