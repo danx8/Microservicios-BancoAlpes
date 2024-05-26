24a78fe4db4c93f70a4138766922279d9e362de8
danielfelipetorreslopez <danielfelipetorreslopez@gmail.com>
2024-05-26 10:34:23 -0500

    web_templates

--- clientes/clientes/settings.py
+++ clientes/clientes/settings.py
@@ -65,7 +65,7 @@ ROOT_URLCONF = 'clientes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['../web_templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
